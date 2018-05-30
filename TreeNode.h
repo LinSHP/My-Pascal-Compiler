9ff88b579372860fe8cb3270da7a2be3e4778cbb
lg998 <gangli@epiclouds.net>
2018-05-30 18:03:53 +0800

    完善了部分语法

--- TreeNode.h
+++ TreeNode.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 
+using namespace std;
+
 enum OperatorType{
     GE,
     GT,
@@ -12,24 +14,62 @@ enum OperatorType{
     OR,
     AND,
     MUL,
-    DIV
+    DIV,
+    BETWEEN,     //1..2这种区间
+    SWITCH_CASE, //case中的 'case 表达式 of'
+    CASE,       //case中的 '常量:语句'
+    FOR,
+    WHILE
 
-}
+};
 
-struct DataType{
+enum DataType{
+    INT,
+    STRING,
+    DOUBLE,
+    CHAR
+};
+
+struct DataValue{
     int _int;
     string _string;
     double _double;
-}
+    char _char;
+};
 
 class TreeNode{
+public:
     vector<TreeNode> childNode;
-    Operator nodeType;
-    DataType data;
+    TreeNode siblingNode = NULL;
+    OperatorType nodeType;
+    DataType dataType;
+    DataValue dataValue;
+
+    TreeNode(OperatorType op){
+        this->nodeType = op;
+    }
 
     TreeNode(TreeNode left, TreeNode right, OperatorType op){
-        this.nodeType = op;
-        this.childNode.push_back(left);
-        this.childNode.push_back(right);
+        this->nodeType = op;
+        this->childNode.push_back(left);
+        this->childNode.push_back(right);
     }
-}
+
+    TreeNode(int data){                     //其实以下的构造方法都是没必要的
+        this->dataType = INT;
+        this->dataValue._int =  data;
+    }
+
+    TreeNode(string data){
+        this->dataType = STRING;
+        this->dataValue._string =  data;
+    }
+    TreeNode(double data){
+        this->dataType = DOUBLE;
+        this->dataValue._double =  data;
+    }
+    TreeNode(char data){
+        this->dataType = CHAR;
+        this->dataValue._char = data;
+    }
+};
